ad73b2faa428b8793d833a12c0cf33f3b87f79c6
Alexandr Gromov <66443586+Tomahawk01@users.noreply.github.com>
2025-12-02 06:57:38 +0100

    Added color to VertexData

--- GameLib/src/Graphics/VertexData.h
+++ GameLib/src/Graphics/VertexData.h
@@ -1,14 +1,16 @@
 #pragma once
 
 #include "Math/Vector3.h"
+#include "Color.h"
 
 namespace Game {
 
 	struct VertexData
 	{
 		vec3 position;
+		Color color;
 	};
 
-	static_assert(sizeof(VertexData) == sizeof(float) * 3);
+	static_assert(sizeof(VertexData) == sizeof(float) * 3 + sizeof(float) * 3);
 
 }
