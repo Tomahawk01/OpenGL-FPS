9824b2ba4103b697c4cf2d6bd382ed9873264acc
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-12-05 01:00:21 -0800

    Improved buffer interface

--- GameLib/src/Graphics/Buffer.h
+++ GameLib/src/Graphics/Buffer.h
@@ -4,20 +4,22 @@
 #include "Utils/DataBuffer.h"
 #include "OpenGL.h"
 
+#include <string_view>
+
 namespace Game {
 
 	class Buffer
 	{
 	public:
-		Buffer(uint32_t size);
+		Buffer(size_t size, std::string_view name);
 
 		void Write(DataBufferView data, size_t offset) const;
 
 		GLuint GetNativeHandle() const;
 
 	private:
-		AutoRelease<::GLuint> m_Buffer;
-		uint32_t m_Size;
+		AutoRelease<GLuint> m_Buffer;
+		size_t m_Size;
 	};
 
 }
