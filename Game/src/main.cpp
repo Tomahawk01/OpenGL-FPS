21a9a029c14f00d6cf000b8b0c825233080eb964
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-12-07 17:14:39 -0800

    Refactor main to use Renderer

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -1,66 +1,13 @@
 #include "Game/config.h"
 #include "Graphics/Window.h"
 #include "Graphics/Shader.h"
-#include "Graphics/Program.h"
-#include "Graphics/Buffer.h"
+#include "Graphics/Scene.h"
 #include "Graphics/CommandBuffer.h"
 #include "Graphics/MeshManager.h"
-#include "Graphics/Scene.h"
+#include "Graphics/Renderer.h"
 #include "Utils/Formatter.h"
 #include "Utils/Log.h"
 #include "Utils/SystemInfo.h"
-
-using namespace std::literals;
-
-namespace {
-
-	constexpr auto sampleVertexShader = R"(
-	#version 460 core
-	
-	struct VertexData
-	{
-		float position[3];
-		float color[3];
-	};
-
-	layout(binding = 0, std430) readonly buffer vertices
-	{
-		VertexData data[];
-	};
-
-	vec3 get_position(int index)
-	{
-		return vec3(data[index].position[0], data[index].position[1], data[index].position[2]);
-	}
-
-	vec3 get_color(int index)
-	{
-		return vec3(data[index].color[0], data[index].color[1], data[index].color[2]);
-	}
-
-	layout(location = 0) out vec3 out_color;
-
-	void main()
-	{
-		gl_Position = vec4(get_position(gl_VertexID), 1.0);
-		out_color = get_color(gl_VertexID);
-	}
-	)"sv;
-
-	constexpr auto sampleFragmentShader = R"(
-	#version 460 core
-	
-	layout(location = 0) in vec3 in_color;
-
-	layout(location = 0) out vec4 out_color;
-
-	void main()
-	{
-		out_color = vec4(in_color, 1.0);
-	}
-	)"sv;
-
-}
 
 int main()
 {
@@ -72,12 +19,8 @@ int main()
 	auto window = Game::Window{ Game::WindowMode::WINDOWED, 1920u, 1080u, 0u, 0u };
 	auto running = true;
 
-	const auto sampleVert = Game::Shader{ sampleVertexShader, Game::ShaderType::VERTEX, "sample_vertex_shader"sv };
-	const auto sampleFrag = Game::Shader{ sampleFragmentShader, Game::ShaderType::FRAGMENT, "sample_fragment_shader"sv };
-	const auto sampleProg = Game::Program{ sampleVert, sampleFrag, "sample_prog"sv };
-
 	auto meshManager = Game::MeshManager{};
-	auto commandBuffer = Game::CommandBuffer{};
+	auto renderer = Game::Renderer{};
 
 	auto scene = Game::Scene{ .entities = {}, .meshManager = meshManager };
 
@@ -92,13 +35,6 @@ int main()
 		{{-0.5f, 0.5f, 0.0f}, Game::Color{0.42f, 0.42f, 0.42f}},
 		{{0.0f, 0.5f, 0.0f}, Game::Color{0.6f, 0.1f, 0.0f}} })
 	});
-
-	auto dummyVAO = Game::AutoRelease<GLuint>{ 0u, [](auto e) { glDeleteVertexArrays(1, &e); } };
-	glGenVertexArrays(1, &dummyVAO);
-
-	glBindVertexArray(dummyVAO);
-	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, meshManager.GetNativeHandle());
-	sampleProg.Use();
 
 	while (running)
 	{
@@ -123,13 +59,7 @@ int main()
 			event = window.PollEvent();
 		}
 
-		const auto commandCount = commandBuffer.Build(scene);
-
-		glBindBuffer(GL_DRAW_INDIRECT_BUFFER, commandBuffer.GetNativeHandle());
-
-		glMultiDrawArraysIndirect(GL_TRIANGLES, nullptr, commandCount, 0);
-
-		commandBuffer.Advance();
+		renderer.Render(scene);
 
 		window.Swap();
 	}
