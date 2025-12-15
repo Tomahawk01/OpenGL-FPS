ea0a032804f79e5c45b3bde9fdfa69971d5962d9
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-12-14 16:41:41 -0800

    Added ResizeGPUBuffer util function

---
+++ GameLib/src/Graphics/Utils.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Utils/DataBuffer.h"
+#include "Utils/Log.h"
+
+#include <vector>
+#include <string_view>
+
+namespace Game {
+
+	template<class T>
+	concept IsBuffer = requires(T t, DataBufferView data, size_t offset)
+	{
+		{ t.Write(data, offset) };
+	};
+
+	template<class T, IsBuffer Buffer>
+	void ResizeGPUBuffer(const std::vector<T>& cpuBuffer, Buffer& gpuBuffer, std::string_view name)
+	{
+		const auto bufferSizeBytes = cpuBuffer.size() * sizeof(T);
+		if (gpuBuffer.GetSize() <= bufferSizeBytes)
+		{
+			auto newSize = gpuBuffer.GetSize() * 2;
+			while (newSize < bufferSizeBytes)
+			{
+				newSize *= 2;
+			}
+
+			Game::Log::Info("Growing {} buffer {} -> {}", name, gpuBuffer.GetSize(), newSize);
+
+			// OpenGL barrier in case gpu using previous frame
+			glFinish();
+
+			gpuBuffer = Buffer{ newSize, name };
+		}
+	}
+
+}
