30378f70d9840723af4a5c08d9772f67ca3cc4b7
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-12-29 15:17:29 -0800

    Bump version

--- premake5.lua
+++ premake5.lua
@@ -3,7 +3,7 @@ dofile("scripts/utils.lua")
 configure_file("scripts/config.h.in", "GameLib/src/config.h", {
     OpenGL_FPS_VERSION_MAJOR = "0",
     OpenGL_FPS_VERSION_MINOR = "0",
-    OpenGL_FPS_VERSION_PATCH = "17"
+    OpenGL_FPS_VERSION_PATCH = "18"
 })
 
 workspace "OpenGL_FPS"
