fda9bcc6b0cb6cf1a846698df815953e66aa9be9
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-12-29 15:16:51 -0800

    Added UV to vertex data

--- GameLib/src/Graphics/VertexData.h
+++ GameLib/src/Graphics/VertexData.h
@@ -5,11 +5,18 @@
 
 namespace Game {
 
+	struct UV
+	{
+		float s;
+		float t;
+	};
+
 	struct VertexData
 	{
 		vec3 position;
+		UV uv;
 	};
 
-	static_assert(sizeof(VertexData) == sizeof(float) * 3);
+	static_assert(sizeof(VertexData) == sizeof(float) * 3 + sizeof(float) * 2);
 
 }
