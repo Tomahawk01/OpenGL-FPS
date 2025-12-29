101ce4d28c1c90d5fb337891c9f9dbb7a0b1f3d1
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-12-29 15:17:13 -0800

    Integrate changes

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -1,5 +1,6 @@
 #include "config.h"
 #include "Core/Scene.h"
+#include "Resources/FileResourceLoader.h"
 #include "Graphics/Window.h"
 #include "Graphics/Shader.h"
 #include "Graphics/Renderer.h"
@@ -14,6 +15,15 @@
 
 namespace {
 
+	template<class ...Args>
+	std::vector<Game::VertexData> Vertices(Args&&... args)
+	{
+		return std::views::zip_transform(
+			[]<class ...A>(A&&... a) { return Game::VertexData{ std::forward<A>(a)... }; },
+			std::forward<Args>(args)...
+		) | std::ranges::to<std::vector>();
+	}
+
 	Game::MeshData Cube()
 	{
 		const Game::vec3 positions[] = {
@@ -25,6 +35,12 @@ namespace {
 			{-1.0f, -1.0f, 1.0f}, {-1.0f, -1.0f, -1.0f}, {1.0f, -1.0f, -1.0f}, {1.0f, -1.0f, 1.0f}
 		};
 
+		const Game::UV uvs[] = {
+			{0.0f, 0.0f}, {1.0f, 0.0f}, {1.0f, 1.0f}, {0.0f, 1.0f}, {0.0f, 0.0f}, {1.0f, 0.0f}, {1.0f, 1.0f}, {0.0f, 1.0f},
+			{0.0f, 0.0f}, {1.0f, 0.0f}, {1.0f, 1.0f}, {0.0f, 1.0f}, {0.0f, 0.0f}, {1.0f, 0.0f}, {1.0f, 1.0f}, {0.0f, 1.0f},
+			{0.0f, 0.0f}, {1.0f, 0.0f}, {1.0f, 1.0f}, {0.0f, 1.0f}, {0.0f, 0.0f}, {1.0f, 0.0f}, {1.0f, 1.0f}, {0.0f, 1.0f},
+		};
+
 		const std::vector<uint32_t> indices = {
 			0, 1, 2, 2, 3, 0,
 			4, 5, 6, 6, 7, 4,
@@ -34,10 +50,7 @@ namespace {
 			20, 21, 22, 22, 23, 20
 		};
 
-		return { .vertices = positions | std::views::transform([](const auto& e)
-												 {
-													 return Game::VertexData{ .position = e };
-												 }) | std::ranges::to<std::vector>(),
+		return { .vertices = Vertices(positions, uvs),
 				 .indices = std::move(indices)
 		};
 	}
@@ -88,6 +101,12 @@ int main()
 	auto window = Game::Window{ Game::WindowMode::WINDOWED, 1920u, 1080u, 0u, 0u };
 	auto running = true;
 
+	auto resourceLoader = Game::FileResourceLoader{ "assets" };
+	const auto diamondFloorAlbedoData = resourceLoader.LoadDataBuffer("textures\\diamond_floor_albedo.png");
+	const auto diamondFloorAlbedo = Game::LoadTexture(diamondFloorAlbedoData);
+	const auto sampler = Game::Sampler{ Game::FilterType::LINEAR, Game::FilterType::LINEAR, "simple_sampler" };
+	const auto diamondFloorTexture = Game::Texture{ diamondFloorAlbedo, "diamond_floor", sampler };
+
 	auto meshManager = Game::MeshManager{};
 	auto materialManager = Game::MaterialManager{};
 	auto renderer = Game::Renderer{};
@@ -110,7 +129,8 @@ int main()
 			std::numbers::pi_v<float> / 4.0f,
 			static_cast<float>(window.GetRenderWidth()), static_cast<float>(window.GetRenderHeight()),
 			0.1f, 1000.0f
-		}
+		},
+		.theOneTexture = diamondFloorTexture
 	};
 
 	scene.entities.push_back(
