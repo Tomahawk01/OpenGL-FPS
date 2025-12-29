4b470d1bbde823f9d7c30dd57f5146462287fdad
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-12-29 09:41:20 -0800

    Added ResourceLoader interface

---
+++ GameLib/src/Resources/ResourceLoader.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Utils/DataBuffer.h"
+
+#include <string_view>
+
+namespace Game {
+
+	class ResourceLoader
+	{
+	public:
+		virtual ~ResourceLoader() = default;
+
+		virtual std::string LoadString(std::string_view name) = 0;
+		virtual DataBuffer LoadDataBuffer(std::string_view name) = 0;
+	};
+
+}
