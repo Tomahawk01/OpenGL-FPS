d2fb15c6006442c306b708b67d33f61dc9db5ad4
Alexandr Gromov <alex.gromov.2410@gmail.com>
2026-01-11 23:50:34 -0800

    Added StringMap

---
+++ GameLib/src/Utils/StringMap.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
+namespace Game {
+
+	struct StringHash
+	{
+		using is_transparent = void;
+		size_t operator()(const char* str) const
+		{
+			return std::hash<std::string_view>{}(str);
+		}
+
+		size_t operator()(std::string_view str) const
+		{
+			return std::hash<std::string_view>{}(str);
+		}
+
+		size_t operator()(const std::string& str) const
+		{
+			return std::hash<std::string_view>{}(str);
+		}
+	};
+
+	template<class T>
+	using StringMap = std::unordered_map<std::string, T, StringHash, std::equal_to<>>;
+
+}
